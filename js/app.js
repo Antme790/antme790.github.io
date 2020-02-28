d5ae935e61a10e4e04b9378eed731e51f2f1d502
Antme790 <Antme790@student.liu.se>
2020-02-28 09:40:18 +0100

    Refactored js and fixes

--- js/app.js
+++ js/app.js
@@ -1,93 +1,116 @@
 var button = document.querySelector(".hamburger");
 var menu = document.querySelector(".menuItems");
 var head = document.querySelector("nav");
-var lis = document.querySelectorAll("nav ul li");
+var lis = document.querySelectorAll("#top ul li");
 var logo = document.querySelector(".logo");
+var guide = document.querySelector(".bottomGuide");
+var hero = document.querySelector(".heroInfo");
+var offset = null;
+var offsetOpacity = null;
+var offsetOutput = "";
 
 // Add attributes to menu(because of DOM Bug?)
-menu.style.transform = "translateX(100%)";
-// menu.style.top = (head.clientHeight-2 + "px");
+// menu.style.transform = "translateX(100%)";
 
 button.addEventListener("click", function() {
-    if(menu.style.transform == "translateX(100%)") {
-        menu.style.display = "flex";
-        setTimeout(function() {
-          menu.style.opacity = "0";
-        menu.style.transform = "translateX(0)";
-        menu.style.boxShadow = "-5px 25px 30px -1px rgba(0,0,0,0.1)";
-        menu.style.opacity = "1";
-
-        // Add opacity to all lis
-        for (var i = 0; i < lis.length; i++) {
-            (function (i) {
-              setTimeout(function () {
-                lis[i].style.opacity = "1";
-              }, 300 + (200*i));
-            })(i);
-          };
-        
-        // Add movement to all lis
-        for (var i = 0; i < lis.length; i++) {
-            (function (i) {
-              setTimeout(function () {
-                lis[i].style.transform = "translateY(0)";
-              }, 300 + (200*i));
-            })(i);
-          };
-        // head.style.height = (head.clientHeight + "px");
-        button.style.position = "fixed";
-        button.style.top = (((head.offsetHeight/2)-(button.offsetHeight/2)) + "px");
-        }, 200);
-    } else {
-        menu.style.transform = "translateX(100%)";
-        menu.style.boxShadow = "none";
-        lis.forEach(function(li) {
-            setTimeout(function() {
-                li.style.opacity = "0";
-            }, 610)
-        });
-
-        for (var i = 0; i < lis.length; i++) {
+  if(!menu.classList.contains("menuItems-open")) {
+    menu.classList.remove("DNONE");
+    setTimeout(() => {
+      menu.classList.add("menuItems-open");
+      menu.classList.add("opacity-full");
+    }, 50);
+    setTimeout(function() {
+      // Add opacity to all lis
+      for (var i = 0; i < lis.length; i++) {
           (function (i) {
             setTimeout(function () {
-              lis[i].style.transform = "translateY(-1rem)";
-            }, 601);
+              lis[i].style.opacity = "1";
+            }, 300 + (200*i));
           })(i);
         };
-        button.style.position = "static";
-        setTimeout(function(){
-          menu.style.display = "none";
-        }, 600);
-    }
+      
+      // Add movement to all lis
+      for (var i = 0; i < lis.length; i++) {
+          (function (i) {
+            setTimeout(function () {
+              lis[i].style.transform = "translateY(0)";
+            }, 300 + (200*i));
+          })(i);
+        };
+      }, 200);
+  } else {
+      menu.classList.remove("menuItems-open");
+      setTimeout(() => {
+        menu.classList.remove("opacity-full");
+      }, 600);
+      // Set li opacity back to 0 after menu closes
+      lis.forEach(function(li) {
+          setTimeout(function() {
+              li.style.opacity = "0";
+          }, 610)
+      });
+      // move li back after menu closes
+      for (var i = 0; i < lis.length; i++) {
+        (function (i) {
+          setTimeout(function () {
+            lis[i].style.transform = "translateY(-1rem)";
+          }, 601);
+        })(i);
+      };
+      // Set display:none on menu after it closes
+      setTimeout(function(){
+        if(!menu.classList.contains("menuItems-open")) {
+          menu.classList.add("DNONE");
+        }
+      }, 600);
+  }
 });
 
 // Animate nav on scroll
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollOpacity(), scrollFunction()};
 
 function scrollFunction() {
   // Animate when window scrolls past 100vh
   if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
-    if(head.style.position === "absolute") {
-      button.style.opacity = "0";
-      head.style.position = "fixed";
-      head.style.transform = "translateY(-100%)";
-      head.style.transition = "all 300ms";
-      setTimeout(function() {
-        button.style.transition = "all 300ms";
-        button.style.transitionDelay = "300ms";
-        button.style.opacity = "100";
-        head.style.transform = "translateY(0)";
-        head.style.backgroundColor = "rgba(44, 44, 44, 0.95)";
-      }, 200);
+    if(!head.classList.contains("nav-open")) {
+      // Move nav top top and hide it
+      head.classList.add("nav-fixed");
+      setTimeout(() => {
+        // Add transtion to nav
+        head.classList.add("nav-animation");
+        setTimeout(() => {
+          // Animate nav slide-down
+          head.classList.add("nav-open");
+        }, 300);
+      }, 100);
     }
-    // Animate when window scrolls back to or less than 100vh
+    // Animate when window scrolls back to or less than 100vh-height of nav
   } else if(document.body.scrollTop < window.innerHeight || document.documentElement.scrollTop < window.innerHeight) {
-    button.style.transition = "all 0ms";
-    button.style.transitionDelay = "0ms";
-    button.style.opacity = "100";
-    head.style.transition = "all 0ms";
-    head.style.backgroundColor = "transparent";
-    head.style.position = "absolute";
+    head.classList.remove("nav-open", "nav-animation", "nav-fixed");
+  }
+}
+
+function scrollOpacity() {
+  // Execute if window is not scrolled past 100vh
+  if(document.documentElement.scrollTop < window.innerHeight) {
+    // Convert pixel offset from window top to a percentage value
+    offset =  Math.round((document.documentElement.scrollTop / window.innerHeight)*100);
+    // Multiply the remainder up to target opacity with offset percent
+    offsetOpacity = 0.6 + ((0.35/95)*offset);
+    // Save final output
+    offsetOutput = "rgba(44, 44, 44, " + offsetOpacity + ")"
+    // Apply Output to inline CSS
+    head.style.backgroundColor = offsetOutput;
+
+    guide.style.opacity = (50-offset)/100;
+    if(offset == 0) {
+      guide.style.opacity = "1";
+    }
+    
+    hero.style.opacity = (65-offset)/100;
+    if(offset == 0) {
+      hero.style.opacity = "1";
+    }
   }
 }
 
